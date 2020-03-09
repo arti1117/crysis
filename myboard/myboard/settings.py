4948adbbc1c30b2bb478bba381fc727eda3cd52a
arti <darkmoonz1004@gmail.com>
2020-03-10 07:48:08 +0900

    set database and add 3/10 log

--- myboard/myboard/settings.py
+++ myboard/myboard/settings.py
@@ -77,6 +77,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'mysql': {
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'crysis',
+        'PASSWORD': 'CrYs1s@1004',
+        'database': 'test_crysis',
+        'default-character-set': 'utf8'
     }
 }
 
